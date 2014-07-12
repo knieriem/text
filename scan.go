86a255c6d2443c48308b332c448d873ddb140c96
Michael Teichgräber <mteichgraeber@gmx.de>
2014-07-13 01:15:44 +0200

    scan: add a simple MultiScanner

--- scan.go
+++ scan.go
@@ -32,3 +32,41 @@ func (s *sectionScanner) Scan() (ok bool) {
 func (s *sectionScanner) Text() string {
 	return s.text
 }
+
+type multiScanner struct {
+	c    chan scanLine
+	line scanLine
+}
+type scanLine struct {
+	text string
+	err  error
+}
+
+func MultiScanner(scanners ...Scanner) Scanner {
+	m := new(multiScanner)
+	m.c = make(chan scanLine, 8)
+	for i := range scanners {
+		s := scanners[i]
+		go func() {
+			for s.Scan() {
+				m.c <- scanLine{text: s.Text()}
+			}
+			m.c <- scanLine{err: s.Err()}
+		}()
+	}
+	return m
+}
+
+func (m *multiScanner) Scan() (ok bool) {
+	m.line = <-m.c
+	ok = m.line.err == nil
+	return
+}
+
+func (m *multiScanner) Text() string {
+	return m.line.text
+}
+
+func (m *multiScanner) Err() error {
+	return m.line.err
+}
