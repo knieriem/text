737b0ee00df03126d0343089f30c4d429cabf36c
Michael Teichgräber <mteichgraeber@gmx.de>
2015-09-24 22:30:49 +0200

    rc/tokenize: add Tokenizer.ParseCmdLine for more sophisticated Parsing

--- rc/tokenize.go
+++ rc/tokenize.go
@@ -1,7 +1,8 @@
+// Package rc implements string handling that mimics the style of the Rc shell.
 package rc
 
 import (
-	"strings"
+	"fmt"
 )
 
 // An implementation of Plan 9's tokenize (see
@@ -11,84 +12,313 @@ import (
 // into fields separated by whitespace. Additionally, single quotes
 // are interpreted and do not appear in the output. In a quoted part
 // of the string, whitespace will not create a new field, and two
-// consequtive single quotes will result in one quote in the output.
-func Tokenize(s string) (fields []string) {
-	if n := do(nil, s); n > 0 { // at first run count fields only
-		fields = make([]string, n)
-		do(fields, s)
+// consecutive single quotes will result in one quote in the output.
+func Tokenize(s string) []string {
+	tokens, _, _ := new(Tokenizer).do(s, false)
+	return tokens.fields()
+}
+
+type Tokenizer struct {
+	buf    groupToken
+	Getenv func(string) []string
+}
+
+type CmdLine struct {
+	Assignments EnvMap
+	Fields      []string
+}
+
+// ParseCmdLine is similar to Tokenize in that  a string is separated into fields, and
+// quoted sections are recognized. It also expands variable references, if Tokenizer.Getenv
+// has been set. Any assignments given at the front of a line are parsed into an EnvMap.
+// On success, a CmdLine structure is returned.
+func (tok *Tokenizer) ParseCmdLine(s string) (c *CmdLine, err error) {
+	tokens, nAssign, err := tok.do(s, true)
+	if err != nil {
+		return
+	}
+	//	fmt.Printf("TokenizeCmd: %q\n", s)
+	//	dump(tokens, "	")
+	if tok.Getenv != nil {
+		for i, t := range tokens {
+			tokens[i] = tok.expandEnv(t)
+		}
+	}
+	c = new(CmdLine)
+	c.Fields = tokens.fields()
+	if nAssign != 0 {
+		c.Assignments = make(EnvMap, nAssign)
+		for _, t := range tokens[:nAssign] {
+			a := t.(*assignmentToken)
+			c.Assignments[a.name.String()] = []string{string(a.stringToken)[1:]}
+		}
+		c.Fields = c.Fields[nAssign:]
 	}
 	return
 }
 
-func do(fields []string, s string) (n int) {
-	var (
-		countOnly = fields == nil
+type token interface {
+	String() string
+	setString(string)
+}
 
-		qf      []string
+type stringAdder interface {
+	addString(string)
+}
+
+type groupToken []token
+type stringToken string
+type varRefToken struct {
+	stringToken
+}
+type assignmentToken struct {
+	stringToken
+	name token
+}
+
+func (t assignmentToken) String() string {
+	return t.name.String() + string(t.stringToken)
+}
+
+func (s *stringToken) setString(arg string) {
+	*s = stringToken(arg)
+}
+func (s *stringToken) addString(arg string) {
+	*s += stringToken(arg)
+}
+
+func (s stringToken) String() string {
+	return string(s)
+}
+
+func (list groupToken) String() (s string) {
+	for _, tok := range list {
+		s += tok.String()
+	}
+	return
+}
+func (groupToken) setString(_ string) {}
+
+func (list groupToken) fields() (f []string) {
+	for _, tok := range list {
+		f = append(f, tok.String())
+	}
+	return
+}
+
+func dump(list groupToken, indent string) {
+	for _, t := range list {
+		if sub, ok := t.(groupToken); ok {
+			fmt.Printf("%s%T\n", indent, t)
+			dump(sub, indent+"\t")
+		} else {
+			fmt.Printf("%s%T %v\n", indent, t, t)
+		}
+	}
+}
+
+func (tok *Tokenizer) expandEnv(t token) token {
+	switch x := t.(type) {
+	case groupToken:
+		for i, sub := range x {
+			x[i] = tok.expandEnv(sub)
+		}
+		t = mergeStringTokens(x)
+	case *assignmentToken:
+		x.name = tok.expandEnv(x.name)
+	case *varRefToken:
+		value := tok.Getenv(x.String()[1:])
+		if len(value) == 0 {
+			value = []string{""}
+		}
+		t = new(stringToken)
+		t.setString(value[0])
+	}
+	return t
+}
+
+func mergeStringTokens(list groupToken) token {
+	var prev stringAdder
+	anyMerges := false
+
+	for i, t := range list {
+		if s, ok := t.(stringAdder); ok {
+			if prev == nil {
+				prev = s
+				continue
+			}
+		}
+		if s, ok := t.(*stringToken); ok {
+			prev.addString(string(*s))
+			anyMerges = true
+			list[i] = nil
+		} else {
+			prev = nil
+		}
+	}
+	if !anyMerges {
+		return list
+	}
+	dest := make(groupToken, 0, len(list))
+	for _, t := range list {
+		if t != nil {
+			dest = append(dest, t)
+		}
+	}
+	if len(dest) == 1 {
+		return dest[0]
+	}
+	return dest
+}
+
+func (tok *Tokenizer) do(s string, handleSpecial bool) (fields groupToken, nAssign int, err error) {
+	var (
+		field   groupToken
 		quoting = false
 		wasq    = false
 
 		i0 = -1
 
-		addField = func(f string) {
-			if len(qf) == 0 {
-				fields[n] = f
-			} else {
-				if len(f) > 0 {
-					qf = append(qf, f)
+		countAssign = true
+		seenAssign  = false
+
+		t token
+
+		setText = func(text string) {
+			if t == nil {
+				if len(field) != 0 {
+					if st, ok := field[len(field)-1].(*stringToken); ok {
+						st.addString(text)
+						return
+					}
 				}
-				fields[n] = strings.Join(qf, "")
-				qf = qf[:0]
+				t = new(stringToken)
+			}
+			t.setString(text)
+		}
+		addField = func(iPos int) {
+			if i0 == -1 {
+				return
+			}
+			if countAssign {
+				if seenAssign {
+					nAssign++
+					seenAssign = false
+				} else {
+					countAssign = false
+				}
+			}
+			if setText(s[i0:iPos]); t != nil {
+				if field == nil {
+					fields = append(fields, t)
+				} else {
+					field = append(field, t)
+				}
+			}
+			if field != nil {
+				if len(field) == 1 {
+					fields = append(fields, field[0])
+				} else {
+					fields = append(fields, field)
+				}
+			}
+			field = nil
+			t = nil
+			i0 = -1
+		}
+
+		flushToken = func(iPos int) {
+			defer func() { i0 = iPos }()
+			if i0 == -1 {
+				return
+			}
+			if setText(s[i0:iPos]); t != nil {
+				field = append(field, t)
+				t = nil
 			}
 		}
 	)
 
-	for i, r := range s {
-		switch r {
-		case ' ', '\t', '\r', '\n':
-			if !quoting && i0 != -1 {
-				if !countOnly {
-					addField(s[i0:i])
-				}
-				n++
-				i0 = -1
-			}
+	fields = tok.buf[:0]
 
-		case '\'':
+	for i, r := range s {
+		if r == '\'' {
 			if !quoting {
 				if wasq {
 					i0--
+					wasq = false
 				}
-				if i0 >= 0 && i-i0 > 0 {
-					if !countOnly {
-						qf = append(qf, s[i0:i])
-					}
-				}
-				i0 = i + 1
 				quoting = true
 			} else {
-				if i-i0 > 0 {
-					if !countOnly {
-						qf = append(qf, s[i0:i])
-					}
-				}
-				i0 = i + 1
 				quoting = false
 				wasq = true
-				continue
 			}
+			flushToken(i)
+			i0 = i + 1
+			continue
+		}
+		wasq = false
+		if quoting {
+			continue
+		}
+
+		switch r {
+		case ' ', '\t', '\r', '\n':
+			addField(i)
+			continue
+		}
+		if !handleSpecial {
+			if i0 == -1 {
+				i0 = i
+			}
+			continue
+		}
+
+		switch r {
+		case '$':
+			flushToken(i)
+			t = new(varRefToken)
+		case '^':
+			if i0 == -1 {
+				if fields == nil {
+					err = tokenSyntaxErr(r)
+					return
+				}
+				iLast := len(fields) - 1
+				tPrev := fields[iLast]
+				if g, ok := tPrev.(groupToken); ok {
+					field = g
+				} else {
+					field = groupToken{tPrev}
+				}
+				fields = fields[:iLast]
+			}
+			flushToken(i)
+			i0++
+		case '=':
+			if seenAssign {
+				err = tokenSyntaxErr(r)
+				return
+			}
+			seenAssign = true
+			flushToken(i)
+			a := new(assignmentToken)
+			a.name = field
+			field = nil
+			t = a
+		case '#':
+			addField(i)
+			return
 		default:
 			if i0 == -1 {
 				i0 = i
 			}
 		}
-		wasq = false
 	}
-	if i0 != -1 {
-		if !countOnly {
-			addField(s[i0:])
-		}
-		n++
-	}
+	addField(len(s))
 	return
+}
+
+func tokenSyntaxErr(r rune) error {
+	return fmt.Errorf("token '%c': syntax error", r)
 }
--- rc/tokenize_test.go
+++ rc/tokenize_test.go
@@ -5,18 +5,27 @@ import (
 )
 
 type testSpec struct {
-	input  string
-	fields []string
+	input       string
+	fields      []string
+	assignments EnvMap
+	mustFail    bool
 }
 
-var tokenizeTests = []testSpec{
+var testEnvMap = EnvMap{
+	"mammal": {"squirrel"},
+	"nut":    {"hazelnut"},
+	"foo":    {"bar"},
+	"ar":     {"az"},
+	"ba":     {"fo"},
+}
+
+var commonTests = []testSpec{
 	{
 		input: "jumps over",
 		fields: []string{
 			"jumps", "over",
 		},
-	},
-	{
+	}, {
 		input: "th''''e 'quick br'own 'fox'",
 		fields: []string{
 			"th'e", "quick brown", "fox",
@@ -24,13 +33,101 @@ var tokenizeTests = []testSpec{
 	},
 }
 
+var tokenizeTests = []testSpec{
+	{
+		input: "a $m'a'm$mal ea^t's 'a $nut",
+		fields: []string{
+			"a", "$mam$mal", "ea^ts a", "$nut",
+		},
+	},
+}
+
+var tokenizeCmdTests = []testSpec{
+	{
+		input: "$foo=ba'z' b$ar=$ba^o $bar",
+		fields: []string{
+			"",
+		},
+		assignments: EnvMap{
+			"bar": {"baz"},
+			"baz": {"foo"},
+		},
+	}, {
+		input: "$foo=ba'z' b#foo",
+		fields: []string{
+			"b",
+		},
+		assignments: EnvMap{
+			"bar": {"baz"},
+		},
+	}, {
+		input: "#foo",
+	}, {
+		input: "a #foo",
+		fields: []string{
+			"a",
+		},
+	}, {
+		input: "'$a' $mammal eats a $nut",
+		fields: []string{
+			"$a", "squirrel", "eats", "a", "hazelnut",
+		},
+	}, {
+		input:    "a=b=c foo",
+		mustFail: true,
+	}, {
+		input:    "^a",
+		mustFail: true,
+	},
+}
+
 func TestTokenize(t *testing.T) {
-	for i, test := range tokenizeTests {
-		fields := Tokenize(test.input)
-		for j, f := range fields {
-			if f2 := test.fields[j]; f != f2 {
-				t.Errorf("[%d] mismatch: %q != %q", i, f, f2)
+	for i, test := range append(commonTests, tokenizeTests...) {
+		compareStringSlices(t, test.fields, Tokenize(test.input), "field", i)
+	}
+}
+
+func TestTokenizeCmd(t *testing.T) {
+	tok := new(Tokenizer)
+	tok.Getenv = func(name string) []string {
+		return testEnvMap[name]
+	}
+	for i, test := range append(commonTests, tokenizeCmdTests...) {
+		cmd, err := tok.ParseCmdLine(test.input)
+		if err != nil {
+			if !test.mustFail {
+				t.Error(err)
 			}
+			continue
+		} else if test.mustFail {
+			t.Errorf("[%d] should have failed", i)
+			continue
+		}
+		compareStringSlices(t, test.fields, cmd.Fields, "field", i)
+		if n1, n2 := len(test.assignments), len(cmd.Assignments); n1 != n2 {
+			t.Errorf("[%d] number of assignments don't match: %d != %d", i, n1, n2)
+			continue
+		}
+		for name, val1 := range test.assignments {
+			val2, ok := cmd.Assignments[name]
+			if !ok {
+				t.Errorf("[%d] assignment not present: %s", i, name)
+				continue
+			}
+			compareStringSlices(t, val1, val2, "assignment value", i)
+		}
+	}
+}
+
+func compareStringSlices(t *testing.T, want, have []string, context string, iTest int) {
+	if len(want) != len(have) {
+		t.Errorf("[%d] %s count: %d != %d", iTest, context, len(want), len(have))
+		return
+	}
+	for i, v := range want {
+		if v != have[i] {
+			t.Errorf("[%d] %s mismatch: %q != %q", iTest, context, v, have[i])
+			return
 		}
 	}
 }
