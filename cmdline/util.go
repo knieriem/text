c737cdd792422878efd312aa33a688a835cdaec4
Michael Teichgräber <mteichgraeber@gmx.de>
2018-07-30 23:43:19 +0200

    cmdline: add utilities for single byte arguments and byte slices

---
+++ cmdline/util.go
@@ -0,0 +1,34 @@
+package cmdline
+
+import (
+	"strconv"
+	"strings"
+)
+
+func Atob(v string) (b byte, err error) {
+	i64, err := strconv.ParseUint(v, 0, 8)
+	if err == nil {
+		b = byte(i64)
+	}
+	return
+}
+
+func Argbytes(arg []string) (data []byte, err error) {
+	data = make([]byte, 0, len(arg))
+	for _, a := range arg {
+		if strings.HasPrefix(a, `"`) && strings.HasSuffix(a, `"`) {
+			s, err := strconv.Unquote(a)
+			if err != nil {
+				return nil, err
+			}
+			data = append(data, []byte(s)...)
+		} else {
+			b, err := Atob(a)
+			if err != nil {
+				return nil, err
+			}
+			data = append(data, b)
+		}
+	}
+	return data, nil
+}
