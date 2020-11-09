89742d5be79a00dcdc39b460d66907356a81b08d
Michael Teichgräber <mteichgraeber@gmx.de>
2020-11-09 13:46:41 +0100

    rc: add QuoteCmd, a variant of Quote that does not quote equal signs

--- rc/quote.go
+++ rc/quote.go
@@ -19,21 +19,66 @@ func NeedsQuote(r rune) bool {
 }
 
 func Quote(s string) string {
-	for _, r := range s {
+	return quote(s, "")
+}
+
+func QuoteCmd(s string) string {
+	return quote(s, "=")
+}
+
+func quote(s, unquoted string) string {
+	quotePart := false
+	q := ""
+	addPart := func(part string) {
+		if quotePart {
+			q += `'` + strings.Replace(part, `'`, `''`, -1) + `'`
+			quotePart = false
+		} else {
+			q += part
+		}
+
+	}
+	i0 := 0
+	for i, r := range s {
+		if unquoted != "" {
+			if r >= utf8.RuneSelf {
+				continue
+			}
+			if strings.IndexByte(unquoted, byte(r)) != -1 {
+				if i > i0 {
+					addPart(s[i0:i])
+				}
+				q += string(r)
+				i0 = i + 1
+				continue
+			}
+		}
 		if NeedsQuote(r) {
-			return "'" + strings.Replace(s, "'", "''", -1) + "'"
+			quotePart = true
 		}
 	}
-	return s
+	if len(s) > i0 {
+		addPart(s[i0:])
+	}
+	return q
 }
 
 func Join(list []string) (js string) {
 	if len(list) == 0 {
 		return
 	}
-	js = list[0]
-	for _, s := range list[1:] {
+	for _, s := range list {
 		js += " " + Quote(s)
+	}
+	return
+}
+
+func JoinCmd(list []string) (js string) {
+	if len(list) == 0 {
+		return
+	}
+	for _, s := range list {
+		js += " " + QuoteCmd(s)
 	}
 	return
 }
