4e1e1f3f3af3dd10f08e1dab76891e8d9b08be23
Michael Teichgräber <mteichgraeber@gmx.de>
2026-07-03 22:07:28 +0200

    interp: expr: implement division, add float support; add more tests

--- interp/expr.go
+++ interp/expr.go
@@ -3,7 +3,6 @@ package interp
 import (
 	"errors"
 	"fmt"
-	"log"
 	"strconv"
 	"time"
 )
@@ -27,35 +26,25 @@ var expr = Cmd{
 			sign = -1
 			fallthrough
 		case "+":
-			if a1.kind != a2.kind {
-				log.Println("arg", arg[1], arg[3], a1, a2)
-				return errExprArgTypeMismatch
+			a, err := a1.add(sign, a2)
+			if err != nil {
+				return err
 			}
-			switch a1.kind {
-			case exprValKindDuration:
-				ctx.Println(a1.t + time.Duration(sign)*a2.t)
-			case exprValKindInt:
-				ctx.Println(a1.i + sign*a2.i)
-			}
+			ctx.Println(a.value())
+
 		case "*":
-			if a1.kind == exprValKindDuration {
-				if a2.kind != exprValKindInt {
-					return errExprArgTypeMismatch
-				}
-				ctx.Println(a1.t * time.Duration(a2.i))
+			a, err := a1.mult(a2)
+			if err != nil {
+				return err
 			}
-			if a2.kind == exprValKindDuration {
-				if a1.kind != exprValKindInt {
-					return errExprArgTypeMismatch
-				}
-				ctx.Println(a2.t * time.Duration(a1.i))
+			ctx.Println(a.value())
+
+		case "/":
+			a, err := a1.div(a2)
+			if err != nil {
+				return err
 			}
-			if a1.kind == exprValKindInt {
-				if a2.kind != exprValKindInt {
-					return errExprArgTypeMismatch
-				}
-				ctx.Println(a1.i * a2.i)
-			}
+			ctx.Println(a.value())
 		}
 		return nil
 	},
@@ -64,16 +53,122 @@ var expr = Cmd{
 var errExprArgTypeMismatch = errors.New("argument type mismatch")
 
 type exprVal struct {
-	kind int
+	kind
 
 	i int
+	f float64
 	t time.Duration
 }
 
+type kind int
+
 const (
-	exprValKindInt = iota
+	exprValKindInt kind = iota
+	exprValKindFloat
 	exprValKindDuration
 )
+
+func (v *exprVal) add(sign int, v2 *exprVal) (*exprVal, error) {
+	if v.hasUnit() != v2.hasUnit() {
+		return nil, errExprArgTypeMismatch
+	}
+	switch v.kind {
+	case exprValKindDuration:
+		v.t += time.Duration(sign) * v2.t
+
+	case exprValKindFloat:
+		v.f += float64(sign) * v2.float()
+
+	case exprValKindInt:
+		v.i += sign * v2.i
+	}
+	return v, nil
+}
+
+func (v *exprVal) mult(v2 *exprVal) (*exprVal, error) {
+	if v.hasUnit() && v2.hasUnit() {
+		return nil, errExprArgTypeMismatch
+	}
+	if v2.kind == exprValKindDuration {
+		v, v2 = v2, v
+	}
+	if v.kind == exprValKindDuration {
+		switch v2.kind {
+		case exprValKindInt:
+			v.t = v.t * time.Duration(v2.i)
+		case exprValKindFloat:
+			v.t = time.Duration(float64(v.t) * v2.f)
+		}
+		return v, nil
+	}
+	if v2.kind == exprValKindFloat {
+		v, v2 = v2, v
+	}
+	if v.kind == exprValKindFloat {
+		v.f = v.f * v2.float()
+	} else {
+		v.i *= v2.i
+	}
+	return v, nil
+}
+
+func (v *exprVal) div(v2 *exprVal) (*exprVal, error) {
+	if !v.hasUnit() && v2.hasUnit() {
+		return nil, errExprArgTypeMismatch
+	}
+
+	if v.kind == exprValKindDuration {
+		switch v2.kind {
+		case exprValKindDuration:
+			v.kind = exprValKindInt
+			v.i = int(v.t / v2.t)
+		case exprValKindInt:
+			v.t /= time.Duration(v2.i)
+		case exprValKindFloat:
+			v.t = time.Duration(float64(v.t) / v2.f)
+		}
+		return v, nil
+	}
+
+	if v.kind == exprValKindFloat {
+		v.f /= v2.float()
+	} else {
+		switch v2.kind {
+		case exprValKindInt:
+			v.i /= v2.i
+		case exprValKindFloat:
+			v.kind = v2.kind
+			v.f = float64(v.i) / v2.f
+		}
+	}
+	return v, nil
+}
+
+func (v *exprVal) value() any {
+	switch v.kind {
+	case exprValKindDuration:
+		return v.t
+	case exprValKindFloat:
+		return v.f
+	case exprValKindInt:
+		return v.i
+	}
+	return nil
+}
+
+func (v *exprVal) float() float64 {
+	switch v.kind {
+	case exprValKindInt:
+		return float64(v.i)
+	case exprValKindDuration:
+		return float64(v.t)
+	}
+	return v.f
+}
+
+func (k kind) hasUnit() bool {
+	return k == exprValKindDuration
+}
 
 func parseExprVal(v string) (*exprVal, error) {
 	if v != "0" {
@@ -85,6 +180,10 @@ func parseExprVal(v string) (*exprVal, error) {
 	i, err := strconv.ParseInt(v, 0, 0)
 	if err == nil {
 		return &exprVal{kind: exprValKindInt, i: int(i)}, nil
+	}
+	f, err := strconv.ParseFloat(v, 64)
+	if err == nil {
+		return &exprVal{kind: exprValKindFloat, f: f}, nil
 	}
 	return nil, fmt.Errorf("cannot parse argument %q", v)
 }
